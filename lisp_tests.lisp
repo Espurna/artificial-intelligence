c5148c927d34e24af2fecc7f41f6299282f09195
Pau Soliva <psoliva@naralabs.com>
2017-11-08 21:43:19 +0100

    ex2a

--- lisp_tests.lisp
+++ lisp_tests.lisp
@@ -1,4 +1,4 @@
-(defun heuristica2 (x)
+(defun heuristica (x)
   "Retornar el valor definit a la taula superior depenen de l'entrada."
   (cond
     ((equal x "A") 80)
@@ -11,3 +11,21 @@
     ((equal x "H") 50)
     ((equal x "Z") 0)
     (t 1000)))
+
+
+(setq map '(("A" "B" 4) ("A" "C" 10) ("A" "D" 10) ("A" "E" 15)
+            ("B" "F" 5) ("C" "F" 20) ("D" "F" 10) ("E" "F" 3) ("E" "H" 30)
+            ("F" "G" 35) ("F" "H" 30)
+            ("H" "Z" 2)))
+
+(defun cost (estat1 estat2)
+  "Retorna el cost entre dos estats que estiguin connectats en el graf de la part 1"
+  (cerca estat1 estat2 map))
+
+(defun cerca (estat1 estat2 mapa)
+  (if (null mapa) nil
+    (if (and
+         (equal (caar mapa) estat1)
+         (equal (cadar mapa) estat2))
+      (caddar mapa)
+      (cerca estat1 estat2 (cdr mapa)))))
