2477cd8e05019001ae049f773bdd3d77d28f04b6
Pau Soliva <psoliva@naralabs.com>
2017-11-09 15:43:35 +0100

    funcio cost acabada

--- astar-pac2-part2-b.lisp
+++ astar-pac2-part2-b.lisp
@@ -213,23 +213,23 @@
 (defun heuristica (x)
   "Retornar el valor definit a la taula superior depenen de l'entrada."
   (cond
-    ((equal x "A") 80)
-    ((equal x "B") 90)
-    ((equal x "C") 60)
-    ((equal x "D") 65)
-    ((equal x "E") 70)
-    ((equal x "F") 50)
-    ((equal x "G") 50)
-    ((equal x "H") 50)
-    ((equal x "Z") 0)
-    (t 1000)))
+    ((equal x 'A) 80)
+    ((equal x 'B) 90)
+    ((equal x 'C) 60)
+    ((equal x 'D) 65)
+    ((equal x 'E) 70)
+    ((equal x 'F) 50)
+    ((equal x 'G) 50)
+    ((equal x 'H) 50)
+    ((equal x 'Z) 0)
+    (t nil)))
 
 ;;;;;;;;; Funció de cost -- apartat 2-b PAC2 2017-18-Q1
 
-(setq map '(("A" "B" 4) ("A" "C" 10) ("A" "D" 10) ("A" "E" 15)
-            ("B" "F" 5) ("C" "F" 20) ("D" "F" 10) ("E" "F" 3) ("E" "H" 30)
-            ("F" "G" 35) ("F" "H" 30)
-            ("H" "Z" 2)))
+(setq map '((A B 4) (A C 10) (A D 10) (A E 15)
+            (B F 5) (C F 20) (D F 10) (E F 3) (E H 30)
+            (F G 35) (F H 30)
+            (H Z 2)))
 
 (defun cost (estat1 estat2)
   "Retorna el cost entre dos estats que estiguin connectats en el graf de la part 1"
